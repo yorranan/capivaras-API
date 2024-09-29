09312a110c51531650558cc44e6b9336c3ca4894
Yorranan <yorranan.costa@outlook.com>
2024-09-29 00:32:05 -0300

    created controllers part 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module'
+import { CapybaraController } from './controllers/capybara.controller';
+import { NoteController } from './controllers/note.controller';
+import { HabitatController } from './controllers/habitat.cotroller';
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }), PrismaModule],
+  controllers: [CapybaraController, NoteController, HabitatController],
 })
 export class AppModule {}
